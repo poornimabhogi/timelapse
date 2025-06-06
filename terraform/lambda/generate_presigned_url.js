0d01faeb9b2ae6e03ab47574090a9a055e805227
Sagar Tangudu <poornima.bhogi1@gmail.com>
2025-06-06 00:27:49 -0500

    changing environment

--- terraform/lambda/generate_presigned_url.js
+++ terraform/lambda/generate_presigned_url.js
@@ -1,5 +1,5 @@
-const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3');
-const { getSignedUrl } = require('@aws-sdk/s3-request-presigner');
+import { S3Client, PutObjectCommand } from ('@aws-sdk/client-s3');
+import { getSignedUrl } from ('@aws-sdk/s3-request-presigner');
 
 const s3Client = new S3Client({ region: process.env.AWS_REGION });
 
